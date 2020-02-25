88ea4b61a0a760e2c5cfbe00323680af59e47660
noydono <th.gabillard@laposte.net>
2020-02-25 23:41:18 +0100

    manque multer-config wtf

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-
-    mongoUri:"mongodb+srv://admin:arinfo20@cluster0-uncir.mongodb.net/test?retryWrites=true&w=majority",
-    mongoLocal:"mongodb://localhost:27017/theClaviste"
-
-}
