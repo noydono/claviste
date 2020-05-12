250e3e2989b97a18ef680f849d7fc3a12940ef75
noydono <th.gabillard@laposte.net>
2020-05-12 15:05:05 +0200

    bug article single connection

--- api/controllers/article/articleSingleController.js
+++ api/controllers/article/articleSingleController.js
@@ -14,7 +14,7 @@ module.exports = {
             ArticleReverse = dbArticle.reverse(),
             dbCom = await Com.find({ article_id: req.params.id }),
              Coms = dbCom.reverse(),
-             like = dbUser.like.length          
+             like = dbArticleVerifSingle.like.length          
 
         res.render('articleVerif/single/articleVerifSingle', {
 
