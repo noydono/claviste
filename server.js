01bf2ee19c18fca91a6839ba15ba606db78bc5a1
noydono <th.gabillard@laposte.net>
2021-01-09 15:19:17 +0100

    obli de save avant de push je suis un abruti

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(helmet())
 /*
  *  mongoose
  * * * * * */
-mongoose.connect(keys.mongoUri || process.env.mongoUri, {
+mongoose.connect(process.env.MONGO_URI, {
 
     useNewUrlParser: true,
     useUnifiedTopology: true,
